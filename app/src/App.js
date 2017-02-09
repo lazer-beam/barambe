c5345ccddda67ef8d2042939ea8094c30bdb6de5
EJMason <eliotjunior@gmail.com>
2017-02-08 19:04:09 -0800

    [setup] scaffolding

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        THIS IS WORKING!
+      </div>
+      
+    );
+  }
+}
+
+export default App;
