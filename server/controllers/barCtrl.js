bdd7814f67018facdea68933bc7327827ec9f947
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-13 15:15:22 -0800

    test

--- server/controllers/barCtrl.js
+++ server/controllers/barCtrl.js
@@ -5,7 +5,7 @@ const bars = {
     console.log(`Serving request for ${req.method} where url is ${req.url}`)
     const name = req.params.name
     const bar = barsHelper.getBar(name)
-    res.send(bar)
+    res.send(bar);
   },
 }
 
