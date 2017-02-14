3ff98187400eb402610e2dd02c8cc6f94c4e1fa0
EJMason <eliotjunior@gmail.com>
2017-02-13 16:57:35 -0800

    [feat] bar model created

--- db/models/bar.model.js
+++ db/models/bar.model.js
@@ -1,3 +1,15 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../conn')
 
+const schema = {
+  name: Sequelize.STRING,
+  subDomain: Sequelize.STRING,
+  isOpen: Sequelize.BOOLEAN,
+  lastCall: Sequelize.TIME,
+}
+
+const configs = { timestamps: false }
+
+const Bar = sequelize.define('bar', schema, configs)
+
+module.exports = Bar
