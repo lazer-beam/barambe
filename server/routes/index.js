17745fb03513ecd3465e2f6ffd4480d10940b5a5
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-14 20:21:17 -0800

    Start working on orders tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const barRoutes = require('./barRoutes')
+const ordersRoutes = require('./ordersRoutes')
 
 module.exports = app => {
   app.use('/bar', barRoutes)
+  app.use('/orders', ordersRoutes)
 }
