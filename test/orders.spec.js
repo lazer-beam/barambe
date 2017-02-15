2ffc26c4e4db2d88316c13bc06e06972ea506ba3
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-14 23:34:23 -0800

    [progress] continue on orders spec setup

--- test/orders.spec.js
+++ test/orders.spec.js
@@ -8,6 +8,7 @@ const AddIn = require('../db/models/addInModel')
 const Drink = require('../db/models/drinkModel')
 const Liquor = require('../db/models/liquorModel')
 const Order = require('../db/models/orderModel')
+const Tab = require('../db/models/tabModel')
 
 var port = 1337
 var url = 'http://127.0.0.1:' + port
@@ -18,31 +19,39 @@ describe('Orders Server Functionality', () => {
 
   beforeEach(() => {
     return Drink.create({
-      type: 'beer',
-      name: 'Miller Lite',
-      price: 700
-    }).then(drink => {
-      createdLines.push(drink)
-      return Drink.create({
         type: 'shot',
         name: 'Greygoose',
         price: 800
-      })
-    }).then(drink => {
-      createdLines.push(drink)
-      return Liquor.create({
-        name: 'Greygoose'
+      }).bind({}).then(drink => {
+        createdLines.push(drink)
+        this.cocktailDrink = drink
+        return Tab.create({
+          customerNum: 15,
+          isOpen: true,
+          isTable: false
+        })
+      }).then(tab => {
+        createdLines.push(tab)
+        this.tab = tab;
+        return Order.create({ status: 'open'})
+      }).then(order => {
+        createdLines.push(order)
+        this.order = order
+        return this.cocktailDrink.addOrder(order)
+      }).then(() => {
+        return this.tab.addOrder(this.order)
+      }).then(() => {
+        return Liquor.create({ name: 'Greygoose' })
       }).then(liquor => {
         createdLines.push(liquor)
-        return drink.addLiquor(liquor)
+        return this.cocktailDrink.addLiquor(liquor)
       })
-    })
   })
 
   afterEach(() => {
     return Promise.reduce(createdLines, function(_, line) {
       console.log('line', line.dataValues)
-      line.destroy()
+      // return line.destroy()
     }, null);
   })
 
@@ -57,45 +66,5 @@ describe('Orders Server Functionality', () => {
         })
         .end(done) //use done to tell mocha that async test is done
     }, 0)
-  })
-
-  it('Should add an addIn to the addIn table', done => {
-    // let barName = 'andrewsbar'
-    // let bar = barsHelper.getBar(barName)
-    // expect(bar).to.be.ok
-    // expect(bar).to.be.an('object')
-    // expect(bar.name).to.equal('andrewsbar')
-    // expect(bar.orders.length).to.equal(0)
-    done()
-  })
-
-  xit('Should get all addIns from a drink', done => {
-    let barName = 'andrewsbar'
-    let bar = barsHelper.getBar(barName)
-    expect(bar).to.be.ok
-    expect(bar).to.be.an('object')
-    expect(bar.name).to.equal('andrewsbar')
-    expect(bar.orders.length).to.equal(0)
-    done()
-  })
-
-  xit('Should add a liquor to the liquor table', done => {
-    let barName = 'andrewsbar'
-    let bar = barsHelper.getBar(barName)
-    expect(bar).to.be.ok
-    expect(bar).to.be.an('object')
-    expect(bar.name).to.equal('andrewsbar')
-    expect(bar.orders.length).to.equal(0)
-    done()
-  })
-
-  xit('Should get all liquors from a drink', done => {
-    let barName = 'andrewsbar'
-    let bar = barsHelper.getBar(barName)
-    expect(bar).to.be.ok
-    expect(bar).to.be.an('object')
-    expect(bar.name).to.equal('andrewsbar')
-    expect(bar.orders.length).to.equal(0)
-    done()
   })
 })
