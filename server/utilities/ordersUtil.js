4abd2f093d064d4a03b13464a747c09cb9f88171
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-15 16:17:34 -0800

    [progress] change tableNumber to tableNum & backend orders work

--- server/utilities/ordersUtil.js
+++ server/utilities/ordersUtil.js
@@ -6,12 +6,12 @@ const mapOrders = orders => orders.map(order => order.dataValues)
 const getAllOrdersWithStatusOpen = orders => orders.filter(order => order.status === 'open')
 
 const isTableOrPickup = tabId => {
-  return Tab.findOne({ where: { id: tabId } }).then(tab => tab.dataValues.tableNumber)
+  return Tab.findOne({ where: { id: tabId } }).then(tab => tab.dataValues.tableNum)
 }
 
 const addDeliveryType = orders => {
   return Promise.all(orders.map(order => {
-    return isTableOrPickup(order.id)
+    return isTableOrPickup(order.tabId)
       .then(tableNum => {
         tableNum ? order.tableNum = tableNum : order.pickup = true
         return order
