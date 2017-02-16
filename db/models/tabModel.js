8fe3ca48e32a8fa31ee70b86301484564d87c286
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-15 16:17:34 -0800

    [progress] change tableNumber to tableNum & backend orders work

--- db/models/tabModel.js
+++ db/models/tabModel.js
@@ -4,7 +4,7 @@ const sequelize = require('../conn')
 const schema = {
   customerNum: Sequelize.INTEGER,
   isOpen: Sequelize.BOOLEAN,
-  tableNumber: Sequelize.INTEGER,
+  tableNum: Sequelize.INTEGER,
   subtotal: Sequelize.INTEGER,
   tax: Sequelize.INTEGER,
   tip: Sequelize.INTEGER,
