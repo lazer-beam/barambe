4951f393d2714fcf883407fea965e47ed2b90b0c
EJMason <eliotjunior@gmail.com>
2017-02-16 02:25:17 -0800

    [feat] comment out components, for now

--- app/src/dashboard/Dashboard.js
+++ app/src/dashboard/Dashboard.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div>
-        HERRO!!!!!
-      </div>
-    )
-  }
-}
+// class Dashboard extends Component {
+//   render() {
+//     return (
+//       <div>
+//         HERRO!!!!!
+//       </div>
+//     )
+//   }
+// }
 
-export default Dashboard
+// export default Dashboard
