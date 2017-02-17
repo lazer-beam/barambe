36afda6c2ba5ec49d4f5598d0d86dfe05101dd14
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-16 21:36:23 -0800

    [progress] implement opening tab route

--- test/tabs.open.spec.js
+++ test/tabs.open.spec.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect
 
-require('../server/server')
+const app = require('../server/server.js')
+const request = require('supertest')(app)
 const tabsUtil = require('../server/utilities/tabsUtil')
 const Tab = require('../db/models/tabModel')
 
@@ -45,6 +46,31 @@ describe('Opening A Tab Functionality', () => {
           expect(tab.dataValues.tableNum).to.be.ok
           expect(tab.dataValues.tableNum).to.be.equal(mockTableNum)
         })
+      })
+  })
+
+  it('should open a tab when POSTing /tabs/opentab', () => {
+    const mockUser = {
+      customerNum: 14,
+      tableNum: 17
+    }
+
+    return request
+      .post('/tabs/opentab')
+      .send(mockUser)
+      .expect(200)
+      .expect(res => {
+        expect(res.text).to.contain('Created tab')
+        return Tab.findAll()
+          .then(tabs => {
+            expect(tabs).to.have.length(1)
+            tabs.forEach(tab => {
+              expect(res.text).to.be.equal(`Created tab ${tab.dataValues.id}`)
+              expect(tab.dataValues.customerNum).to.be.equal(mockUser.customerNum)
+              expect(tab.dataValues.tableNum).to.be.ok
+              expect(tab.dataValues.tableNum).to.be.equal(mockUser.tableNum)
+            })
+          })
       })
   })
 })
