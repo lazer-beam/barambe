181cfbaeed7b27ee73651cd25d6488b32fcbbcbf
EJMason <eliotjunior@gmail.com>
2017-02-17 06:24:12 -0800

    [feat] navigation now kinda works

--- app/src/dashboard/DashboardComponents.js
+++ app/src/dashboard/DashboardComponents.js
@@ -1,5 +1,4 @@
 import React from 'react'
 import { Menu, Icon } from 'semantic-ui-react'
 
-// export const MenuItem = ({ icon, label }) => (<Menu.Item name={icon}><Icon name={icon} />{label}</Menu.Item>)
-export default ({ icon, label }) => (<Menu.Item name={icon}><Icon name={icon} />{label}</Menu.Item>)
+export default ({ icon, label, nav }) => (<Menu.Item onClick={(e, data) => nav(e, data, icon)} name={icon}><Icon name={icon} />{label}</Menu.Item>)
--- app/src/dashboard/duck.Dashboard.js
+++ app/src/dashboard/duck.Dashboard.js
@@ -4,6 +4,8 @@
 export const types = {
   TOGGLE_SIDEBAR_OUT: 'DASH/TOGGLE_SIDEBAR_OUT',
   TOGGLE_SIDEBAR_IN: 'DASH/TOGGLE_SIDEBAR_IN',
+  NAV_SELECT_SIDEBAR_ITEM: 'DASH/NAV_SELECT_SIDEBAR_ITEM',
+  NAV_SELECT_DRINKS: 'DASH/NAV_SELECT_DRINKS',
 }
 
 // ========================================
@@ -11,7 +13,7 @@ export const types = {
 // ========================================
 const defaultProps = {
   visible: false,
-  currentNav: 'root',
+  currentNav: 'home',
 }
 
 export default (state = defaultProps, action) => {
@@ -20,6 +22,10 @@ export default (state = defaultProps, action) => {
       return { ...state, visible: true }
     case types.TOGGLE_SIDEBAR_IN:
       return { ...state, visible: false }
+    case types.NAV_SELECT_SIDEBAR_ITEM:
+      return { ...state, currentNav: action.payload }
+    case types.NAV_SELECT_DRINKS:
+      return { ...state, currentNav: 'drinks' }
 
     default:
       return state
@@ -32,4 +38,5 @@ export default (state = defaultProps, action) => {
 export const actions = {
   toggleSidebarOut: () => ({ type: types.TOGGLE_SIDEBAR_OUT }),
   toggleSidebarIn: () => ({ type: types.TOGGLE_SIDEBAR_IN }),
+  navSelection: selection => ({ type: types.NAV_SELECT_SIDEBAR_ITEM, payload: selection }),
 }
