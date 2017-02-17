f39a9a99572329478ff02f6060b1db042e8c7829
Andrew Tran <avtran23@gmail.com>
2017-02-17 15:22:05 -0800

    Create utility fns to retrieve drinks

--- server/utilities/drinksUtil.js
+++ server/utilities/drinksUtil.js
@@ -1,10 +1,49 @@
 const Drink = require('../../db/models/drinkModel')
+const Promise = require('bluebird')
 
 const getAllDrinks = orders => Promise.all(orders.map(order => Drink.findOne({ where: { id: order.drinkId } })))
 
 const findDrinkByName = drinkName => Drink.findOne({ where: { name: drinkName } })
 
+const getAllCocktails = drinkResults => {
+  return Promise.all(drinkResults.map(drink => {
+    const drinkObj = {
+      name: drink.dataValues.name,
+      price: drink.dataValues.price,
+      type: drink.dataValues.type,
+    }
+
+    return drink.getLiquors()
+      .then(liquors => {
+        drinkObj.liquors = liquors.map(liquorObj => liquorObj.dataValues.name)
+        return drink.getAddIns()
+      }).then(addIns => {
+        drinkObj.addIns = addIns.map(addInObj => addInObj.dataValues.name)
+        return drinkObj
+      }).catch(err => {
+        console.log(err)
+      })
+  }))
+}
+
+const getDrinkType = drinkType => {
+  Drink.findAll({ where: { type: drinkType } })
+  .then(drinkResults => {
+    if (drinkType === 'cocktails') {
+      return getAllCocktails(drinkResults)
+    }
+    return drinkResults.map(drink => {
+      return {
+        name: drink.dataValues.name,
+        price: drink.dataValues.price,
+        type: drinkType,
+      }
+    })
+  })
+}
+
 module.exports = {
   getAllDrinks,
   findDrinkByName,
+  getDrinkType,
 }
