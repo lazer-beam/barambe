8cfe05887d9e72baacf87a4aa692043279b5e39c
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-16 20:32:43 -0800

    [progress] implement open a tab utility method

--- test/tabs.open.spec.js
+++ test/tabs.open.spec.js
@@ -1,16 +1,50 @@
 const expect = require('chai').expect
 
+require('../server/server')
 const tabsUtil = require('../server/utilities/tabsUtil')
 const Tab = require('../db/models/tabModel')
 
 describe('Opening A Tab Functionality', () => {
-  it('should open a tab', () => {
-    return tabsUtil.openTab()
+  afterEach(() => {
+    return Tab.findAll()
+      .then(tabs => {
+        tabs.forEach(tab => {
+          tab.destroy()
+        })
+      })
+  })
+
+  it('should open a tab with pickup delivery type', () => {
+    const mockCustomerNum = 17
+
+    return tabsUtil.openTab(mockCustomerNum)
       .then(tab => {
         expect(tab).to.be.ok
         return Tab.findAll()
       }).then(tabs => {
-        expect(tabs).to.have.length(0)
+        expect(tabs).to.have.length(1)
+        tabs.forEach(tab => {
+          expect(tab.dataValues.customerNum).to.be.equal(mockCustomerNum)
+          expect(tab.dataValues.tableNum).to.be.a('null')
+        })
+      })
+  })
+
+  it('should open a tab with table delivery type', () => {
+    const mockCustomerNum = 14
+    const mockTableNum = 3
+
+    return tabsUtil.openTab(mockCustomerNum, mockTableNum)
+      .then(tab => {
+        expect(tab).to.be.ok
+        return Tab.findAll()
+      }).then(tabs => {
+        expect(tabs).to.have.length(1)
+        tabs.forEach(tab => {
+          expect(tab.dataValues.customerNum).to.be.equal(mockCustomerNum)
+          expect(tab.dataValues.tableNum).to.be.ok
+          expect(tab.dataValues.tableNum).to.be.equal(mockTableNum)
+        })
       })
   })
 })
