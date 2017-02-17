30a98c4e77e9ca74ad6200ccb1ffcca7a76ea9aa
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-02-16 21:36:23 -0800

    [progress] implement opening tab route

--- server/controllers/tabsCtrl.js
+++ server/controllers/tabsCtrl.js
@@ -2,9 +2,11 @@ const tabsUtil = require('../utilities/tabsUtil')
 
 const tabs = {
   open: (req, res) => {
-    tabsUtil.openTab()
-      .then(() => {
-        res.send()
+    tabsUtil.openTab(req.body.customerNum, req.body.tableNum)
+      .then(tab => {
+        res.send(`Created tab ${tab.dataValues.id}`)
+      }).catch(err => {
+        res.status(500).send(err)
       })
   },
 }
--- server/routes/tabRoutes.js
+++ server/routes/tabRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const tabsCtrl = require('../controllers/tabsCtrl')
 
-router.get('/opentab', tabsCtrl.tabs.open)
+router.post('/opentab', tabsCtrl.tabs.open)
 
 module.exports = router
