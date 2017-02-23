c52f6fa20cecb06507a776fb310bd57613735965
Andrew Tran <avtran23@gmail.com>
2017-02-22 22:55:24 -0800

    [progress]Create connect button for bar owners
    Once database configured, can save bar stripeID

--- server/routes/barRoutes.js
+++ server/routes/barRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const barCtrl = require('../controllers/barCtrl')
 
-router.get('/getbar/:name', barCtrl.bars.get)
+// router.get('/getbar/:name', barCtrl.bars.get)
 router.get('/connect', barCtrl.bars.connect)
 router.get('/connect/callback', barCtrl.bars.getBarStripeData)
 
