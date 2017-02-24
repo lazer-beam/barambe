8ed6e9410a475bf6c84a88dd16871379daa5d690
Andrew Tran <avtran23@gmail.com>
2017-02-24 02:39:36 -0800

    [fix/feat]Refactor backend payment ctrl and routes

--- server/controllers/customerCtrl.js
+++ server/controllers/customerCtrl.js
@@ -1,47 +1,23 @@
 const stripe = require('stripe')(process.env.testKey)
 
 const customer = {
-  // add SDK interaction later for existing stripe users, change creation logic also
-  // create: (req, res) => {
-  //   console.log(`Serving request for ${req.method} where url is ${req.url}`)
-  //   const authID = req.body.authID
-  //   const token = req.body.token
-  //   stripe.customers.create({
-  //     email: null,
-  //     source: token,
-  //   }).then(newCustomer => {
-  //     // save customerID in database, corresponding to authID
-  //     res.send({ result: 'User created' })
-  //   }).catch(err => {
-  //     res.send(err)
-  //   })
-  // },
+  pay: (req, res) => {
+    console.log(`Serving request for ${req.method} where url is ${req.url}`)
+    stripe.charges.create({
+      amount: req.body.amount,
+      currency: req.body.currency,
+      customer: req.body.stripeID,
+      destination: req.body.barID,
+    }, (err, charge) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send(charge)
+      }
+    })
+  },
 
-  // pay: (req, res) => {
-  //   console.log(`Serving request for ${req.method} where url is ${req.url}`)
-  //   const authID = req.body.authID
-  //   const currency = req.body.currency
-  //   const amount = req.body.amount
-  //   const barID = req.body.barID
-  //   // get the barStripeID using the barAuthID, and use it in the charge creation
-  //   cusHelper.getCusID(authID)
-  //   .then(cusID => {
-  //     stripe.charges.create({
-  //       amount,
-  //       currency,
-  //       customer: cusID,
-  //       // destination: { account: barStripeID }
-  //     })
-  //   })
-  //   .then(chargeRes => {
-  //     res.send(chargeRes)
-  //   })
-  //   .catch(err => {
-  //     res.send(err)
-  //   })
-  // },
-
-  saveCard: (req, res) => {
+  saveInfo: (req, res) => {
     console.log(`Serving request for ${req.method} where url is ${req.url}`)
     console.log(`Req.body: ${req.body}`)
     stripe.customers.create(
