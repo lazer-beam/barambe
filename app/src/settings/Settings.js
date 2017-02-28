79c34f191a38fa98b0958ad7aaf7cefe2675027f
EJMason <eliotjunior@gmail.com>
2017-02-28 08:37:16 -0800

    [feat] bars can now add subdomain to metadata

--- app/src/settings/Settings.js
+++ app/src/settings/Settings.js
@@ -1,44 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import axios from 'axios'
-import { Header, Container, Input, Segment, Icon } from 'semantic-ui-react'
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux'
+// import axios from 'axios'
+// import { Header, Container, Input, Segment, Icon } from 'semantic-ui-react'
 
-@connect(store => ({
-  currentAddView: store.drinks.currentAddView,
-  beers: store.drinks.menuBeers,
-  cocktails: store.drinks.menuCocktails,
-  liquors: store.drinks.menuLiquors,
-  addIns: store.drinks.menuAddIns,
-}))
-class Settings extends Component {
-  test() {
-    
-  }
+// @connect(store => ({
+//   currentAddView: store.drinks.currentAddView,
+//   beers: store.drinks.menuBeers,
+//   cocktails: store.drinks.menuCocktails,
+//   liquors: store.drinks.menuLiquors,
+//   addIns: store.drinks.menuAddIns,
+// }))
+// class Settings extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Container>
+//           <Header as="h3">
+//             <Icon name="settings" />
+//             <Header.Content>
+//               Account Settings
+//               <Header.Subheader>
+//                 Manage your preferences
+//               </Header.Subheader>
+//             </Header.Content>
+//           </Header>
+//           <Header as="h2" attached="top">
+//             Bar Username
+//           </Header>
+//           <Segment attached>
+//             <p> This is a unique username. Once it is set it cannot be changed.</p>
+//             <Input action={{ icon: 'search' }} placeholder="Search..." />
+//           </Segment>
+//         </Container>
+//       </div>
+//     )
+//   }
+// }
 
-  render() {
-    return (
-      <div>
-        <Container>
-          <Header as="h3">
-            <Icon name="settings" />
-            <Header.Content>
-              Account Settings
-              <Header.Subheader>
-                Manage your preferences
-              </Header.Subheader>
-            </Header.Content>
-          </Header>
-          <Header as="h2" attached="top">
-            Bar Username
-          </Header>
-          <Segment attached>
-            <p> This is a unique username. Once it is set it cannot be changed.</p>
-            <Input action={{ icon: 'search' }} placeholder="Search..." />
-          </Segment>
-        </Container>
-      </div>
-    )
-  }
-}
-
-export default Settings
+// export default Settings
